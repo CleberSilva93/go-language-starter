de44b8b07b44f3ca01fe3cd239c56d9f649b27e8
CleberAugustz <cleber.augustz@hotmail.com>
2022-11-26 06:29:40 -0300

    chore: validate name and if null throw error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,21 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == ""{
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }
 
 func Ola(name string) string{
--- hello/hello.go
+++ hello/hello.go
@@ -3,15 +3,33 @@ package main
 import (
 	"fmt"
 	"go-starter/greetings"
+	"log"
 )
 
 func main(){
 	fmt.Println("Hello world :D")
 
-	message := greetings.Hello("Gladys")
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetingsError: ")
+    log.SetFlags(0)
 
+    // Request a greeting message.
+	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+
+	// Call other function from same 
 	messagept := greetings.Ola("Gladys")
 
-	fmt.Println(message)
 	fmt.Println(messagept)
 }
