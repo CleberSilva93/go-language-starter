1695a516fdfaa669878afbd040c9bb96a365750b
CleberAugustz <cleber.augustz@hotmail.com>
2022-11-26 06:37:52 -0300

    chore: random phrase to print

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
@@ -13,7 +15,8 @@ func Hello(name string) (string, error) {
 
 	// If a name was received, return a value that embeds the name
 	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    // Create a message using a random format.
+    message := fmt.Sprintf(randomFormat(), name)
 
 	return message, nil
 }
@@ -22,4 +25,26 @@ func Ola(name string) string{
 	message := fmt.Sprintf("Olá, %v. Seja bem-vindo!", name)
 
 	return message
+}
+
+// init sets initial values for variables used in the function.
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string{
+  // A slice of message formats.
+  formats := []string{
+	"Hi, %v. Welcome!",
+	"Great to see you, %v!",
+	"Hail, %v! Wall met!",
+  }
+
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+
+    return formats[rand.Intn(len(formats))]
 }
